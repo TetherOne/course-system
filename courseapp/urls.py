c3b2981a6587d05bcf86fc3242fd386b4568d94d
Pavel Ryaguzov <pashajobber@gmail.com>
2024-03-03 13:48:32 +0500

    ad ViewSet's

--- courseapp/urls.py
+++ courseapp/urls.py
@@ -1,12 +1,17 @@
-from courseapp.views import main_page
+from courseapp.views import main_page, CoursesViewSet, TeacherProfilesViewSet
 
-from django.urls import path
-
-
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
 app_name = 'courseapp'
 
 
+routers = DefaultRouter()
+routers.register('courses', CoursesViewSet,)
+routers.register('teacher-profiles', TeacherProfilesViewSet,)
+
+
 urlpatterns = [
     path('main/', main_page, name='main-page'),
+    path('api/', include(routers.urls)),
 ]
--- courseapp/views.py
+++ courseapp/views.py
@@ -3,7 +3,21 @@ from django.http import HttpRequest
 
 from django.shortcuts import render
 
+from rest_framework.viewsets import ModelViewSet
+
+from courseapp.models import Course, TeacherProfile
+from courseapp.serializers import CoursesSerializer, TeacherProfileSerializer
 
 
 def main_page(request: HttpRequest) -> HttpResponse:
     return render(request, 'courseapp/main-page.html')
+
+
+class CoursesViewSet(ModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CoursesSerializer
+
+
+class TeacherProfilesViewSet(ModelViewSet):
+    queryset = TeacherProfile.objects.all()
+    serializer_class = TeacherProfileSerializer
