4b5859c23f1eb30f8dafb500dcbdcbf413609a69
Pavel Ryaguzov <pashajobber@gmail.com>
2024-03-03 13:47:56 +0500

    delete apiapp

--- apiapp/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from courseapp.models import TeacherProfile
-
-
-class TeacherProfileSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = TeacherProfile
-        fields = '__all__'
