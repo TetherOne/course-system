94e2a0f8d09aceadf1b7d6b22bfab3078f5415e1
Pavel Ryaguzov <pashajobber@gmail.com>
2024-03-05 20:34:28 +0500

    ad StudentProfile model

--- courseapp/models.py
+++ courseapp/models.py
@@ -16,6 +16,20 @@ class TeacherProfile(models.Model):
     )
 
 
+class StudentProfile(models.Model):
+
+    id = models.AutoField(primary_key=True)
+    surname = models.CharField(max_length=100, blank=True, null=True)
+    father_name = models.CharField(max_length=100, blank=True, null=True)
+    faculty = models.CharField(max_length=100, blank=True, null=True)
+    group = models.CharField(max_length=100, blank=True, null=True)
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name='student_profile',
+    )
+
+
 class Course(models.Model):
 
     id = models.AutoField(primary_key=True)
