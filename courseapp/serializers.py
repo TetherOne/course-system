d7305486deb9a41ac5a52939e6559b719e74eda3
Pavel Ryaguzov <pashajobber@gmail.com>
2024-03-05 20:35:24 +0500

    ad StudentProfileSerializer

--- courseapp/serializers.py
+++ courseapp/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 
 from .models import TeacherProfile
+from .models import StudentProfile
+
 from .models import Course
 
 
@@ -13,4 +15,10 @@ class CoursesSerializer(serializers.ModelSerializer):
 class TeacherProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = TeacherProfile
+        fields = '__all__'
+
+
+class StudentProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = StudentProfile
         fields = '__all__'
