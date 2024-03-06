53ea1d026c2cc96c4b16274695194858815edd97
Pavel Ryaguzov <pashajobber@gmail.com>
2024-03-06 17:21:20 +0500

    update admin

--- courseapp/admin.py
+++ courseapp/admin.py
@@ -1,4 +1,4 @@
-from courseapp.models import TeacherProfile
+from courseapp.models import TeacherProfile, Video
 from courseapp.models import StudentProfile
 from courseapp.models import Course
 
@@ -30,12 +30,26 @@ class StudentProfileAdmin(admin.ModelAdmin):
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
 
-    list_display = 'id', 'description', 'teacher_profile'
-    list_display_links = 'id', 'description'
-    search_fields = 'description',
-    list_filter = 'description', 'teacher_profile'
+    list_display = 'id', 'name', 'description', 'teacher_profile'
+    list_display_links = 'id', 'name'
+    search_fields = 'name',
+    list_filter = 'teacher_profile',
     ordering = 'id',
     list_per_page = 10
 
     def get_queryset(self, request):
         return Course.objects.select_related('teacher_profile')
+
+
+@admin.register(Video)
+class VideoAdmin(admin.ModelAdmin):
+
+    list_display = 'id', 'description', 'course'
+    list_display_links = 'id', 'description'
+    search_fields = 'description',
+    list_filter = 'course',
+    ordering = 'id',
+    list_per_page = 10
+
+    def get_queryset(self, request):
+        return Video.objects.select_related('course')
