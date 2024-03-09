7c3483d4ef8bd8be7f9a37588bc770db35220b19
Pavel Ryaguzov <pashajobber@gmail.com>
2024-03-09 22:32:47 +0500

    ad JWT authorization

--- course_system/urls.py
+++ course_system/urls.py
@@ -21,12 +21,14 @@ from django.contrib import admin
 
 from django.urls import include
 from django.urls import path
-
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("courseapp.urls")),
     path('api/auth/', include('rest_framework.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
 ]
 
