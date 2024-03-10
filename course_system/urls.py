4b31e83035572f4ffadb07f5a87de92c749c3297
Pavel Ryaguzov <pashajobber@gmail.com>
2024-03-10 13:22:22 +0500

    delete JWT

--- course_system/urls.py
+++ course_system/urls.py
@@ -28,9 +28,6 @@ from django.urls import path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("courseapp.urls")),
-    path('api/auth/', include('rest_framework.urls')),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
 
