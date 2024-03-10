20525da6524ffb50488359842668f7a8890fc02a
Pavel Ryaguzov <pashajobber@gmail.com>
2024-03-10 15:21:03 +0500

    update path

--- course_system/urls.py
+++ course_system/urls.py
@@ -23,11 +23,11 @@ from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib import admin
 
-from django.urls import path
-
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('api/', include('courseapp.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
