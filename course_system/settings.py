94d81ea3c1ddd40512dbc120e7d23a968b6eb833
Pavel Ryaguzov <pashajobber@gmail.com>
2024-03-11 12:51:03 +0500

    ad JWT-auth

--- course_system/settings.py
+++ course_system/settings.py
@@ -152,7 +152,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
 }
 
 
