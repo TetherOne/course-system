65fa353bc18ba2bd708da210d2d651e250d2b375
Pavel Ryaguzov <pashajobber@gmail.com>
2024-03-15 12:48:28 +0500

    ad password to course

--- courseapp/models.py
+++ courseapp/models.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.contrib.auth.models import User
 
 from django.db import models
@@ -65,9 +67,15 @@ class Course(models.Model):
         null=True,
         related_name="courses",
     )
+    course_password = models.CharField(max_length=50, default='', blank=True)
 
     def __str__(self):
         return f"{self.course_name}"
+
+    def save(self, *args, **kwargs):
+        if not self.course_password:
+            self.course_password = str(uuid.uuid4())[:8].replace('-', '')
+        super().save(*args, **kwargs)
 
 
 def course_video_directory_path(instance: "Video", filename: str) -> str:
