81b78607b834252e6babc69d52cf4348c3ea0514
Pavel Ryaguzov <pashajobber@gmail.com>
2024-03-17 13:14:30 +0500

    create conftest

---
+++ tests/api/conftest.py
@@ -0,0 +1,49 @@
+from rest_framework.test import APIClient
+
+import pytest
+
+
+@pytest.fixture
+def client():
+    return APIClient()
+
+
+teacher_payload = {
+    "email": "teacher@example.com",
+    "username": "teacher_username",
+    "password": "test123",
+    "is_teacher": True
+}
+
+
+student_payload = {
+    "email": "student@example.com",
+    "username": "student_username",
+    "password": "test123",
+    "is_teacher": False
+}
+
+
+course_payload = {
+    "course_name": "Test Course",
+    "description": "Test course description",
+    "course_password": "coursepass"
+}
+
+
+lesson_payload = {
+    "lesson_name": "Test Lesson",
+    "video": "",
+    "description": "Test lesson description"
+}
+
+
+checkpoint_payload = {
+    "title": "Test Checkpoint",
+}
+
+
+question_payload = {
+    "question_text": "",
+    "max_points": 1
+}
