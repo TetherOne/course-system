3ac85e5e823fa0fdac232e30a7d7cd0c397944d5
Pavel Ryaguzov <pashajobber@gmail.com>
2024-03-17 13:14:16 +0500

    update test_users

---
+++ tests/api/test_users.py
@@ -0,0 +1,31 @@
+from tests.api.conftest import client, teacher_payload, student_payload
+
+import pytest
+
+
+@pytest.mark.django_db
+def test_register_teacher(client):
+
+    response = client.post("/api/userapp/users/", teacher_payload)
+
+    data = response.data
+
+    assert data["username"] == teacher_payload["username"]
+    assert data["email"] == teacher_payload["email"]
+    assert "password" not in data
+    assert data["is_teacher"] is True
+    assert response.status_code == 201
+
+
+@pytest.mark.django_db
+def test_register_student(client):
+
+    response = client.post("/api/userapp/users/", student_payload)
+
+    data = response.data
+
+    assert data["username"] == student_payload["username"]
+    assert data["email"] == student_payload["email"]
+    assert "password" not in data
+    assert data["is_teacher"] is False
+    assert response.status_code == 201
