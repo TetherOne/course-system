9d52d9672c862c8a332138c1c73c0a98a239d660
Pavel Ryaguzov <pashajobber@gmail.com>
2024-03-23 23:12:14 +0500

    ad scripts

--- front_vue/src/components/pages-vue/Course.vue
+++ front_vue/src/components/pages-vue/Course.vue
@@ -1,51 +1,4 @@
-<script setup>
-
-</script>
-
-<script>
-
-
-import {getCourseInfo, getModuleVideos, getTeacherInfo} from "../../requests.js";
-import axios from "axios";
-import {frontURL} from "../../config.js";
-
-export default {
-    data() {
-        return {
-            id: this.$route.params.id,
-            info: {},
-            teacherInfo: {},
-            modules: []
-        }
-    },
-    async created() {
-        this.info = await getCourseInfo(this.id);
-        this.teacherInfo = await getTeacherInfo(this.info.teacher_profile);
-
-
-        axios.get(`${frontURL}/api/courseapp/modules/?course=${this.id}&format=json`).then(
-            response => {
-                this.modules = response.data;
-                let i = 1;
-                this.modules.forEach(lesson => {
-                    lesson.number = i++;
-                })
-            }
-        ).then(
-            _ => {
-                this.modules.forEach(async module => {
-                    module.videos = await getModuleVideos(module.id);
-                })
-            }
-        )
-    },
-    methods: {
-        toggleVideos(index) {
-            this.modules[index].showVideos = !this.modules[index].showVideos;
-        }
-    },
-}
-</script>
+<!Doctype html>
 
 <template>
     <div id="main-wrapper" class="flex-column">
@@ -90,6 +43,12 @@ export default {
         </div>
     </div>
 </template>
+
+
+<script src="../pages-scripts/course.js">
+
+</script>
+
 
 <style src="../pages-css/course.css">
 
---
+++ front_vue/src/components/pages-vue/Lesson.vue
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+<template>
+    <div class="tittle">
+        <a @click="goBack">
+            <img src="/src/assets/arrow.png" alt="Arrow" class="arrow-image">
+            <span class="back-text">Назад</span>
+        </a>
+        <span class="lesson-name">
+            <span>{{ name }}</span>
+        </span>
+    </div>
+
+    <div class="lesson-container">
+        <video class="lesson-video" controls :src="video"></video>
+    </div>
+</template>
+
+
+<script src="../pages-scripts/lesson.js">
+
+</script>
+
+
+<style src="../pages-css/lesson.css">
+
+</style>
