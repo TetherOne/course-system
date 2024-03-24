8e308087e1e298ab583a463325ca89e3c0496c58
Pavel Ryaguzov <pashajobber@gmail.com>
2024-03-24 13:16:59 +0500

    update

--- front_vue/src/components/pages-vue/Lesson.vue
+++ front_vue/src/components/pages-vue/Lesson.vue
@@ -1,31 +1,36 @@
 <!DOCTYPE html>
 
 <template>
-    <div class="tittle-container">
-        <a @click="goBack" class="arrow-text">
-            <img src="/src/assets/arrow.png" alt="Arrow" class="arrow-image">
-            <span class="back-text">Назад</span>
-        </a>
-        <span class="lesson-name">
-            <span>{{ name }}</span>
-        </span>
-    </div>
+    <header>
+        <div class="tittle-container">
+            <a @click="goBack" class="arrow-text">
+                <img src="/src/assets/arrow.png" alt="Arrow" class="arrow-image">
+                <span class="back-text">Назад</span>
+            </a>
+            <span class="lesson-name">
+                <span>{{ name }}</span>
+            </span>
+        </div>
+    </header>
 
-    <div class="lesson-container">
-        <div>
-            <video class="lesson-video" controls :src="video"></video>
-            <div class="lesson-details">
-                <div class="lesson-tittle-description">Описание</div>
-                <div class="lesson-description">{{ description }}</div>
-                <div class="lesson-tittle-materials">Материалы</div>
-                <div class="lesson-materials-container">
-                    <div v-for="(file, index) in otherFiles" :key="index">
-                        <a class="lesson-material" :href="file" target="_blank">{{ shortenLink(file) }}</a>
+    <body>
+        <div class="lesson-container">
+            <div>
+                <video class="lesson-video" controls :src="video"></video>
+                <div class="lesson-details">
+                    <div class="lesson-tittle-description">Описание</div>
+                    <div class="lesson-description">{{ description }}</div>
+                    <div class="lesson-tittle-materials">Материалы</div>
+                    <div class="lesson-materials-container">
+                        <div v-for="(file, index) in otherFiles" :key="index">
+                            <a class="lesson-material" :href="file" target="_blank">{{ shortenLink(file) }}</a>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
+    </body>
+
 </template>
 
 
