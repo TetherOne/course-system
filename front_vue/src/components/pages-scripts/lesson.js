4698c80412a6c9f214735bd3e32f3bfcc367de57
k0vvasuwa <k0vvasuwa@yandex.ru>
2024-03-24 11:04:31 +0500

    f

--- front_vue/src/components/pages-scripts/lesson.js
+++ front_vue/src/components/pages-scripts/lesson.js
@@ -1,27 +1,39 @@
-import { getLesson } from "../../requests.js";
+import {getLesson, getLessonOtherFiles} from "../../requests.js";
 
 export default {
     data() {
-      return {
-        name: '__lessonName__',
-        description: '__lessonDescription__',
-        video: '__videoPath__'
-      }
+        return {
+            name: '__lessonName__',
+            description: '__lessonDescription__',
+            video: '__videoPath__',
+            otherFiles: [] // links
+        }
     },
     computed: {
-      id() {
-        return this.$route.params.lessonId;
-      }
+        courseId() {
+            return this.$route.params.courseId;
+        },
+        id() {
+            return this.$route.params.lessonId;
+        },
+        lessonNumber() {
+            return `${this.courseId}. ${this.id}.`;
+        }
     },
     async created() {
-      const lesson = await getLesson(this.id);
-      this.name = lesson.lesson_name;
-      this.description = lesson.description;
-      this.video = lesson.video;
+        const lesson = await getLesson(this.id);
+        this.name = lesson.lesson_name;
+        this.description = lesson.description;
+        this.video = lesson.video;
+
+        const otherFiles = await getLessonOtherFiles(this.id);
+        otherFiles.forEach(file => {
+            this.otherFiles.push(file.other_file);
+        });
     },
     methods: {
-      goBack() {
-        this.$router.go(-1);
-      }
+        goBack() {
+            this.$router.go(-1);
+        }
     }
-  }
+}
