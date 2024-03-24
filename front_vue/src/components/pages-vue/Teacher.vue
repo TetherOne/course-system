989c0594f14f26076bb05c6fc73d1f3dff6552a9
k0vvasuwa <k0vvasuwa@yandex.ru>
2024-03-24 16:09:12 +0500

    f

--- front_vue/src/components/pages-vue/Teacher.vue
+++ front_vue/src/components/pages-vue/Teacher.vue
@@ -1,5 +1,4 @@
-
-<script setup>
+Ка<script setup>
 import { useUserStore } from '../../stores/user.js';
 
 
@@ -17,8 +16,34 @@ const user = useUserStore();
 </template>
 
 
-<script src="../pages-scripts/teacher.js">
+<script>
+import {
+    getTeacherCourses,
+    getTeacherInfo
+} from '../../requests.js';
 
+
+export default {
+    data() {
+        return {
+            id: this.$route.params.id,
+            avatar: '__avatar__',
+            surname: '__surname__',
+            name: '__name__',
+            fatherName: '__fatherName__',
+            courses: []
+        }
+    },
+    async created() {
+        const teacherInfo = await getTeacherInfo(this.id);
+        this.surname = teacherInfo.surname;
+        this.name = teacherInfo.name;
+        this.fatherName = teacherInfo.father_name;
+        this.avatar = teacherInfo.avatar;
+
+        this.courses = await getTeacherCourses(this.id);
+    }
+}
 </script>
 
 
