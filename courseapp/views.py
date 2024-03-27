ec9e38522bf7b2d78bc2c6b9658545a93c1260a1
Pavel Ryaguzov <pashajobber@gmail.com>
2024-03-27 19:48:34 +0500

    ad cache to enrollment api

--- courseapp/views.py
+++ courseapp/views.py
@@ -6,14 +6,31 @@ from courseapp.serializers import LessonSerializer
 from courseapp.serializers import CourseSerializer
 from courseapp.serializers import ModuleSerializer
 
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+
+from django.db.models.signals import post_delete
+from django.db.models.signals import post_save
 
 from rest_framework.filters import OrderingFilter
 
 from rest_framework.viewsets import ModelViewSet
 
-from courseapp.models import Enrollment, Lesson, LessonOtherFile
+from courseapp.models import LessonOtherFile
+from courseapp.models import Enrollment
+from courseapp.models import Lesson
 from courseapp.models import Course
 from courseapp.models import Module
+
+from django.dispatch import receiver
+
+from django.core.cache import cache
+
+
+@receiver(post_save, sender=Enrollment,)
+@receiver(post_delete, sender=Enrollment,)
+def clear_enrollment_cache(sender, instance, **kwargs):
+    cache.clear()
 
 
 class EnrollmentViewSet(ModelViewSet):
@@ -22,6 +39,10 @@ class EnrollmentViewSet(ModelViewSet):
     serializer_class = EnrollmentSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ["student", "course"]
+
+    @method_decorator(cache_page(60 * 2, key_prefix='enrollments'))
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
 
 
 class CourseViewSet(ModelViewSet):
