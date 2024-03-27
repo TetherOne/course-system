9ca0372f5976c1efa69e75697b1fb7db891147ca
Pavel Ryaguzov <pashajobber@gmail.com>
2024-03-27 22:04:04 +0500

    ad logging

--- course_system/settings.py
+++ course_system/settings.py
@@ -202,3 +202,21 @@ CACHES = {
         "LOCATION": "redis://127.0.0.1:6379",
     }
 }
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django.db.backends': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+    },
+}
