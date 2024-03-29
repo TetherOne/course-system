eecb8c0ccaea1883215bf6767cf2cf87389437c5
Pavel Ryaguzov <pashajobber@gmail.com>
2024-03-29 21:30:06 +0500

    ad passwords for mail

---
+++ config.py
@@ -0,0 +1,13 @@
+from dotenv import load_dotenv
+
+import os
+
+
+load_dotenv()
+
+
+EMAIL_HOST = os.environ.get("EMAIL_HOST")
+EMAIL_PORT = os.environ.get("EMAIL_PORT")
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+EMAIL_USE_SSL = os.environ.get("EMAIL_USE_SSL")
