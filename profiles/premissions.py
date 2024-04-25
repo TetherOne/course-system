3e8d66ceb0626a08e933dc9fe7113c208da820e8
Pavel Ryaguzov <pashajobber@gmail.com>
2024-04-25 11:33:25 +0500

    update permission for users

--- profiles/premissions.py
+++ profiles/premissions.py
@@ -2,7 +2,15 @@ from rest_framework.permissions import BasePermission
 
 
 class IsOwnerOrReadOnly(BasePermission):
+
+    def has_permission(self, request, view):
+        if request.method in ["GET"]:
+            return request.user.is_authenticated
+        return True
+
     def has_object_permission(self, request, view, obj):
         if request.method in ["POST", 'PUT', 'PATCH', 'DELETE']:
             return obj.user == request.user or request.user.is_staff
+        elif request.method == 'GET':
+            return request.user.is_authenticated
         return True
