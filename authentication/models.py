5940b13ec6195910f395f4ac2c943b0ce2eccf51
Pavel Ryaguzov <pashajobber@gmail.com>
2024-05-09 23:26:31 +0500

    ad russians name

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,5 @@
+from django.utils.translation import gettext_lazy as _
+
 from django.contrib.auth.models import AbstractUser
 
 from typing import TYPE_CHECKING
@@ -8,8 +10,8 @@ from django.db import models
 
 class CustomUser(AbstractUser):
 
-    email = models.EmailField(unique=True)
-    username = models.CharField(max_length=30, unique=True)
+    email = models.EmailField(_("почта"), unique=True)
+    username = models.CharField(_("никнейм"), max_length=30, unique=True)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["username"]
