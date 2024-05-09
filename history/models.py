1275cd4158afeb007ef3413a76f912d8beb28ff6
Pavel Ryaguzov <pashajobber@gmail.com>
2024-05-09 23:27:08 +0500

    ad russians name

--- history/models.py
+++ history/models.py
@@ -1,7 +1,9 @@
-from typing import TYPE_CHECKING
+from django.utils.translation import gettext_lazy as _
 
-from django.db import models
 from django.db.models import Manager
+from django.db import models
+
+from typing import TYPE_CHECKING
 
 
 class HistoryOfPassedAnswer(models.Model):
@@ -10,25 +12,29 @@ class HistoryOfPassedAnswer(models.Model):
         "profiles.StudentProfile",
         on_delete=models.SET_NULL,
         null=True,
+        verbose_name=_("студент"),
     )
     checkpoint = models.ForeignKey(
         "checkpoints.CheckPoint",
         on_delete=models.SET_NULL,
         null=True,
+        verbose_name=_("контрольная точка"),
     )
     question = models.ForeignKey(
         "questions.Question",
         on_delete=models.SET_NULL,
         null=True,
+        verbose_name=_("вопрос"),
     )
     selected_answer = models.ForeignKey(
         "questions.Answer",
         on_delete=models.SET_NULL,
         null=True,
+        verbose_name=_("выбранный ответ"),
     )
-    attempt_number = models.IntegerField(default=1)
-    is_correct = models.BooleanField(default=False)
-    points = models.IntegerField(default=0)
+    attempt_number = models.IntegerField(_("попытка"), default=1)
+    is_correct = models.BooleanField(_("правильно ли"), default=False)
+    points = models.IntegerField(_("баллы"), default=0)
 
     class Meta:
         db_table = "history_of_passed_answers"
