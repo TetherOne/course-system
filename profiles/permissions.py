fe15f2f820bf1f59f025251541f21a9d5ba7c1a6
Pavel Ryaguzov <pashajobber@gmail.com>
2024-05-16 17:41:07 +0500

    update permission fr profiles

--- profiles/permissions.py
+++ profiles/permissions.py
@@ -5,14 +5,12 @@ class IsOwnerOrReadOnly(BasePermission):
 
     def has_permission(self, request, view):
         if request.method in SAFE_METHODS:
-            # return request.user.is_authenticated
             return True
         return True
 
     def has_object_permission(self, request, view, obj):
         if request.method in ["POST", "PUT", "PATCH", "DELETE"]:
-            return obj.user == request.user or request.user.is_staff
+            return obj.user == request.user
         elif request.method == "GET":
-            # return request.user.is_authenticated
             return True
         return True
