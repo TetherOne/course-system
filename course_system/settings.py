8c21e32d4286a47b00024e332eb35d9cfce7382a
Pavel Ryaguzov <pashajobber@gmail.com>
2024-05-20 19:14:10 +0500

    update static

--- course_system/settings.py
+++ course_system/settings.py
@@ -156,13 +156,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [BASE_DIR / "templates/static"]
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
-
-STATICFILES_DIRS = [BASE_DIR / "templates/static"]
 
 
 # Default primary key field type
