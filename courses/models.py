39bbca45b1343405c55963e4a51f21320e337602
Pasha Ryaguzov <pashajobber@gmail.com>
2024-05-23 16:23:58 +0500

    delete automatic generate password

--- courses/models.py
+++ courses/models.py
@@ -1,8 +1,10 @@
+import string
 import uuid
 from typing import TYPE_CHECKING
 
 from django.db import models
 from django.db.models import Manager
+from django.utils.crypto import get_random_string
 from django.utils.translation import gettext_lazy as _
 
 from courses.utils import lesson_video_directory_path, other_file_directory_path
@@ -87,6 +89,14 @@ class Course(models.Model):
 
     if TYPE_CHECKING:
         objects: Manager
+
+    def save(self, *args, **kwargs):
+        if not self.course_password:
+            self.course_password = get_random_string(
+                6,
+                string.ascii_letters + string.digits,
+            )
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.course_name}"
